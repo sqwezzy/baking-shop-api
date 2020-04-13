cb56b5dec668cdc0823825db69c94db55937ba89
sqwezzy <maks.subbotko46@gmail.com>
2020-04-13 11:19:01 +0300

    Update dish controller

--- src/controllers/dishController.ts
+++ src/controllers/dishController.ts
@@ -50,22 +50,15 @@ async function deleteDish(req: Request, res: Response) {
 }
 
 async function updateDish(req: Request, res: Response) {
+    const updateDish = req.body
+    if (req.file) {
+        req.body.img = req.file.path;
+    }
     try {
-        console.log(req.file.path);
         const dish = await Dish.findOneAndUpdate(
             { _id: req.params.id },
             {
-                $set: {
-                    name: req.body.name,
-                    price: parseInt(req.body.price),
-                    rating: parseInt(req.body.rating),
-                    categoryCode: parseInt(req.body.categoryCode),
-                    img: req.file.path,
-                    weight: parseInt(req.body.weight),
-                    composition: req.body.composition,
-                    description: req.body.description,
-                }
-            },
+                $set: updateDish},
             { new: true }
         );
         res.status(200).json(dish)
